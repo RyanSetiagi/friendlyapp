ea0c0bdf6cbf447ff2d60e857c501592bb0578a9
Ryan Setiagi <setiagi.ryan@gmail.com>
2018-03-02 09:11:30 +0700

    exercise continued

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ void main(){
   runApp(new MainApp());
 }
 
+const String _name = "MasbOssun";
+
 class MainApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
@@ -20,6 +22,7 @@ class MainWidget extends StatefulWidget {
 }
 
 class ChatWidget extends State<MainWidget>{
+  final List<MessageWidget> _pesan = <MessageWidget>[];
   final TextEditingController _isiPesan = new TextEditingController();
 
   Widget _KolomPesan(){
@@ -52,6 +55,12 @@ class ChatWidget extends State<MainWidget>{
 
   void _submitKlik(String text){
     _isiPesan.clear();
+      MessageWidget pesan = new MessageWidget(
+        text: text,
+      );
+      setState(() {
+        _pesan.insert(0, pesan);
+      });
   }
 
   @override
@@ -60,7 +69,55 @@ class ChatWidget extends State<MainWidget>{
       appBar: new AppBar(
         title: new Text("Chat"),
       ),
-      body: _KolomPesan(),
+      body: new Column(
+        children: <Widget>[
+          new Flexible(
+              child: new ListView.builder(
+                padding: new EdgeInsets.all(8.0),
+                reverse: true,
+                itemBuilder: (_, int index) => _pesan[index],
+                itemCount: _pesan.length,
+              ),
+          ),
+          new Divider(height: 1.0),
+          new Container(
+            decoration: new BoxDecoration(
+              color: Theme.of(context).cardColor),
+            child: _KolomPesan(),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MessageWidget extends StatelessWidget {
+  MessageWidget({this.text});
+  final String text;
+  @override
+  Widget build(BuildContext context){
+    return new Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: new CircleAvatar(
+              child: new Text(_name[0]),),
+          ),
+          new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Text(_name, style: Theme.of(context).textTheme.subhead,),
+              new Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: new Text(text),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
